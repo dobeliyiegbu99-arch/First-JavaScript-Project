f2a1c858a9a0fc0149b8b0a32f18626f494d8ade
Dobel <dobeliyiegbu99@gmail.com>
2025-09-17 01:13:09 -0700

    firstjs

--- myScript.js
+++ myScript.js
@@ -1,47 +1,41 @@
 let fname = "Dobel"
+console.log(fname);
 
 const favouriteNumber = 10;
+console.log(favouriteNumber);
 
 let a = 5;
 let b = 10;
+let temp = a;
+a = b;
+b = temp;
 
 let x = 7;
 let y = 5;
 let sum = x + y;
+console.log(sum)
 
 let age = 22;
 let futureAge = age + 10;
+console.log(futureAge)
 
 let temperature = 25;
+console.log("Today's temperature is " + temperature + " deg. Cel")
 
 let firstname = "Dobel ";
 let lastname = "Iyiegbu ";
 let fullname = firstname + lastname;
+console.log(fullname);
 
 const length = 5;
 const width = 8;
 let area = length * width;
+console.log(area);
 
 const daysInWeek = 7;
+console.log("There are " + daysInWeek + " days in a week");
 
 let balance = 100;
 let deposit = 50;
 let newBalance = balance + deposit;
-
-console.log(fname);
-
-console.log(favouriteNumber);
-
-console.log(sum)
-
-console.log(futureAge)
-
-console.log("Today's temperature is " + temperature + " deg. Cel")
-
-console.log(fullname);
-
-console.log(area);
-
-console.log("There are " + daysInWeek + " days in a week");
-
 console.log(newBalance);
